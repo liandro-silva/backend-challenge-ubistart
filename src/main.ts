1db68bed0f2b9920d4ad3b1a709aaa803dbdaaf4
Liandro Silva <liandro.silva1012@gmail.com>
2022-07-23 22:13:11 -0300

    feat: ensure basic safety requirements

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get<EnvironmentVariables>(ConfigService);
+  app.use(helmet);
+  app.use(csurf());
   await app.listen(configService.port);
 }
 bootstrap();
